e514b7f6b9281d2f5fc0af902d7c37fa4b81b24e
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2021-05-09 11:09:03 -0400

    write text on canvas

--- BubbleSort/BubbleSort/bubbleSort.py
+++ BubbleSort/BubbleSort/bubbleSort.py
@@ -1,10 +1,21 @@
 import time 
 
-def bubbleSort(data, dibujar):
+
+def bubbleSort(data, dibujar, cnv):
+    cnv.create_text(100,200,text="Inciando bubble sort...") 
     for _ in range(len(data)-1): 
         for j in range(len(data)-1):
             if data[j] > data[j+1]:
-                data[j],data[j+1] = data[j+1], data[j]
+                
+                cnv.delete("all") 
+                a = (("El numero " + str(data[j]) + " es mayor que " + str(data[j+1])))
+                cnv.create_text(100,100,text=a)
+                 
+                
+                data[j],data[j+1] = data[j+1], data[j] 
+                b = (("Intercambiando de lugar " + str(data[j]) + " con " + str(data[j+1])))
+                cnv.create_text(100,120,text=b)
+
                 dibujar(data, 
                     [
                     'green' 
@@ -12,7 +23,8 @@ def bubbleSort(data, dibujar):
                     else 'red' for x in range(len(data))
                     ]
                 )
-                time.sleep(1)
+                time.sleep(2)
     dibujar(data, ['green' for x in range(len(data))])
-
     
+    cnv.delete("all")
+    cnv.create_text(100,200,text="Array ordenado con Bubble Sort")
